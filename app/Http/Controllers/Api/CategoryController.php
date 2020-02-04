e46250e3acc53760d8b33c82a308de73e8e2e4af
kooler62 <kooler62@gmail.com>
2020-02-04 23:52:04 +0200

    scaffold category resource

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return CategoryResource::collection($categories);
+    }
+
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+}
