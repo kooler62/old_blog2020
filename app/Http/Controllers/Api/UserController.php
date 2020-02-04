d03df69cf005aa1a26f41fc6712992fa2fcd167f
kooler62 <kooler62@gmail.com>
2020-02-04 23:34:09 +0200

    scaffold user resource

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return UserResource::collection($users);
+    }
+
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+}
