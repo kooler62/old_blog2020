702b9639d662ce03460636db2a551153787b9def
kooler62 <kooler62@gmail.com>
2020-02-08 13:40:43 +0200

    optimization

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Category;
-use App\Post;
+use App\{Category, Post};
 
 class CategoryController extends Controller
 {
@@ -17,10 +16,13 @@ class CategoryController extends Controller
 
     public function show(string $slug)
     {
-        $category = Category::whereSlugAndActive($slug, 1)->firstOrFail();
-        $posts = Post::publicPosts()->where('category_id', $category->id)->paginate(10);
-      //  dd($posts);
-        //dd($posts->first());
+        $category = cache()->remember("category-$slug", now()->addDays(30), function () use($slug) {
+            return Category::whereSlugAndActive($slug, 1)->firstOrFail();
+        });
+
+        $posts = cache()->remember("category_posts-$slug-".request()->page, now()->addMinutes(60), function() use($category){
+            return Post::publicPosts()->where('category_id', $category->id)->paginate(10);
+        });
         return view('category', compact('category', 'posts'));
     }
 }
